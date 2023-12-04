a458efa9e5c295bb126e7dd620a7ab820692ff52
Maik Larooij <mklarooij@gmail.com>
2023-12-04 09:57:36 +0100

    Day 3 done!

---
+++ puzzle_3a/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let grid: Vec<Vec<char>> = include_str!("../input.txt").lines().map(|line| line.chars().collect()).collect();
+    let n: usize = grid.len();
+
+    let mut curr_num: String = String::new();
+    let mut curr_valid: bool = false;
+    let mut total_sum: u32 = 0;
+
+    for row in 0..n {
+        for col in 0..n {
+
+            let c = grid[row][col];
+            
+            if c.is_numeric() {
+                curr_num = format!("{}{}", curr_num, c);
+                
+                // Check neighbors
+                for row_offset in [-1, 0, 1] {
+                    for col_offset in [-1, 0, 1] {
+                        let row_to_check: usize = (row as isize + row_offset as isize) as usize;
+                        let col_to_check: usize = (col as isize + col_offset as isize) as usize;
+                        
+                        // Out of bounds / overflow
+                        if row_to_check >= n || col_to_check >= n {
+                            continue;
+                        }
+                        
+                        // Check if the current cell is a symbol
+                        let to_check = grid[row_to_check][col_to_check];
+                        if !to_check.is_numeric() && to_check != '.' {
+                            curr_valid = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            // Check total found number
+            if !c.is_numeric() || col == n - 1  {
+                if curr_valid {
+                    println!("Found valid number: {}", curr_num);
+                    total_sum += curr_num.parse::<u32>().unwrap();
+                }
+
+                curr_num = String::new();
+                curr_valid = false;
+            }
+        }
+    }
+
+    println!("Total sum: {}", total_sum);
+    
+}   
